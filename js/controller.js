4b7f3412766a2e363c7a5eef49d6aeba5b9bb8d3
abelalvarez89 <abelalvarez89@hotmail.com>
2015-06-09 17:33:24 -0400

    Full Class Complete

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,17 @@
-var artistController = angular.module('artistController', []);
 
-artistController.controller('ListController', ['$scope', '$http' , function($scope, $http) {
-	$http.get('js/data.json').success(function(data) {
-		$scope.artists = data;
-		$scope.artistOrder = 'name';
+var artistControllers = angular.module('artistControllers', []);
 
-	});
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.artists = data;
+    $scope.artistOrder = 'name';
+  });
 }]);
 
+artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.artists = data;
+    $scope.whichItem = $routeParams.itemId;
+  });
+}]);
 
